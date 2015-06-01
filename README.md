93e4c175c9224545271f067dcc46a8f46e9071e8
Elliot Wright <wright.elliot@gmail.com>
2015-06-01 11:10:25 +0100

    Updated proxy to allow all environments to be proxied.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# td-tdapi-proxy
+# TD API Proxy
+
+To set up the proxy, make sure you have node and npm installed, then run the following while in the 
+root of the repository.
+
+```
+$ npm install -g pm2
+$ npm install
+$ pm2 startup
+$ pm2 start ./main.js
+```
--- main.js
+++ main.js
@@ -3,7 +3,45 @@
 var app = require("express")();
 var proxy = require("express-http-proxy");
 
-app.use("/proxy", proxy("https://remoteservices.tddirectinvesting.co.uk", {
+app.use("/proxy-live", proxy("https://remoteservices.tddirectinvesting.co.uk", {
+    forwardPath: function(req, res) {
+        return require("url").parse(req.url).path;
+    },
+    intercept: function(rsp, data, req, res, callback) {
+        delete res._headers["www-authenticate"];
+        delete res._headerNames["www-authenticate"];
+
+        if (res._headers["set-cookie"]) {
+            res._headers["set-cookie"][0] = res._headers["set-cookie"][0]
+                .replace("; Secure", "")
+                .replace("; HTTPOnly", "")
+            ;
+        }
+
+        callback(null, data);
+    }
+}));
+
+app.use("/proxy-preprod", proxy("https://pptdw.tdwh-test.co.uk", {
+    forwardPath: function(req, res) {
+        return require("url").parse(req.url).path;
+    },
+    intercept: function(rsp, data, req, res, callback) {
+        delete res._headers["www-authenticate"];
+        delete res._headerNames["www-authenticate"];
+
+        if (res._headers["set-cookie"]) {
+            res._headers["set-cookie"][0] = res._headers["set-cookie"][0]
+                .replace("; Secure", "")
+                .replace("; HTTPOnly", "")
+            ;
+        }
+
+        callback(null, data);
+    }
+}));
+
+app.use("/proxy-test", proxy("https://ev1tdw.tdwh-test.co.uk", {
     forwardPath: function(req, res) {
         return require("url").parse(req.url).path;
     },
